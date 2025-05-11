8e291439f23c9b09b3436e3a5dca240d5db7cc3a
unknown <bijoordzen@gmail.com>
2025-05-11 23:34:12 +0530

    sping initialize

---
+++ src/main/java/com/example/LibSwiftCore/LibSwiftCoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.LibSwiftCore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibSwiftCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibSwiftCoreApplication.class, args);
+	}
+
+}
